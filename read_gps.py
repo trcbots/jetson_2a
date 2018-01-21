b1bc497f66a03ee1afd9f66ace33c144e49ba357
hroachewilson <hroachewilson@gmail.com>
2018-01-21 20:04:05 +1000

    Updated name for serial port used in program

--- read_gps.py
+++ read_gps.py
@@ -1,33 +1,31 @@
 import serial
 import pynmea2
 import sys
-ser = serial.Serial()
+serial_gps = serial.Serial()
 
-ser.port = '/dev/ttyUSB0'
-ser.baudrate = 9600
-ser.open()
+serial_gps.port = '/dev/ttyUSB0'
+serial_gps.baudrate = 9600
+serial_gps.open()
 
-# CONFIG MACROS
+# GPS CONFIG MACROS
 PMTK_SET_NMEA_BAUDRATE = '$PMTK251,9600*17'
-PMTK_SET_NMEA_OUTPUT_RMCONLY = '$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29'
 PMTK_SET_NMEA_UPDATE_5HZ = "$PMTK220,200*2C"
+PMTK_SET_NMEA_OUTPUT_RMCONLY = '$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29'
 PMTK_SET_NMEA_OUTPUT_RMCGGA = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
 PMTK_SET_NMEA_OUTPUT_GGAONLY = "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
 NUM_SATS_NEEDED = 8
 
 
-# CONFIG ROUTINE
-ser.write(PMTK_SET_NMEA_BAUDRATE + '\r\n')
-ser.write(PMTK_SET_NMEA_OUTPUT_GGAONLY + '\r\n')
-ser.write(PMTK_SET_NMEA_UPDATE_5HZ + '\r\n')
+# GPS CONFIG ROUTINE
+serial_gps.write(PMTK_SET_NMEA_BAUDRATE + '\r\n')
+serial_gps.write(PMTK_SET_NMEA_OUTPUT_GGAONLY + '\r\n')
+serial_gps.write(PMTK_SET_NMEA_UPDATE_5HZ + '\r\n')
 
-# Note: when using GGAONLY, dollar sign must be prepended to gps line
 
 while True:
 
-
-	for line in ser.read():
-		line = ser.readline()
+	for line in serial_gps.read():
+		line = serial_gps.readline()
 		isline = False
 		try: # try statement so that GGAONLY doesn't catch the initial line and crash
 			msg = pynmea2.parse(line, check=True)
